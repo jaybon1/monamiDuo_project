7dd428736d7557b7b4663a71b7d849d46add7829
hyosun <andy703744@gmail.com>
2020-07-05 21:27:51 +0900

    20200705

--- monami_portfolio/src/com/monami/repository/ItemsRepository.java
+++ monami_portfolio/src/com/monami/repository/ItemsRepository.java
@@ -111,6 +111,7 @@ public class ItemsRepository {
 		return null; // 실패시
 	}
 	
+	
 	// 파트별로 가져오기 (새로운 기능을 추가할 때는 새로 액션도 새로 추가 하기)
 	public List<Items> find20ItemsByPage(int page, String value) { // object 받기(안에 내용 다 받아야 하니까)
 		final String SQL = 
